fe31acb5bbc045f9ce2bfa486a558ba400f8c244
Soledad B <122616795+Solcito22@users.noreply.github.com>
2023-07-12 13:36:07 -0300

    Calcular el mínimo

---
+++ CalcularMinimo.java
@@ -0,0 +1,19 @@
+
+public class CalcularMinimo {
+	
+	public static int minimo(int[] lista) {
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < lista.length; i++) {
+			if (lista[i] < min) {
+				min = lista[i];
+			}
+		}
+		return min;
+	}
+
+	public static void main(String[] args) {
+		int[] lista = {9, 3, 2, 7, 8};
+		System.out.println(minimo(lista));
+	}
+
+}
